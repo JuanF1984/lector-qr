8a5268b45c406bec88b41a488371de1411b4fe54
JuanF1984 <juanferreyra2684@gmail.com>
2026-01-09 10:49:21 -0300

    Se agrega PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ["icons/icono.png"],
       manifest: {
         name: 'Lector QR',
-        short_name: 'QR Reader',
+        short_name: 'QR Lector',
         description: 'Lector de códigos QR profesional con historial',
         theme_color: '#1e1b4b',
         background_color: '#0f0f23',
@@ -23,43 +22,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'icons/icono.png',
             sizes: '192x192',
             type: 'image/png'
           },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
+         
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 año
-              }
-            }
-          }
-        ]
-      }
+          navigateFallback: "/index.html",
+      },
     }),
   ],
-  server: {
-    host: true, // Para acceder desde el celular en desarrollo
-    port: 5173
-  }
+ 
 })
